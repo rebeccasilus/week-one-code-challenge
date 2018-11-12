2057a3cf0d22624f305a624c06e4a56ca67635d5
Rebecca Silus <rebeccasilus@gmail.com>
2018-11-12 14:13:29 -0600

    changes red to yellow

---
+++ client.js
@@ -0,0 +1,30 @@
+$( document ).ready(readyNow);
+
+let clickCounter = 0;
+
+function readyNow() {
+    console.log('jq');
+    $('#button').on('click', createDiv);
+    $('')
+} // end readyNow
+
+function createDiv(){
+    //console.log('new div');
+    let counterP = $('.counter');
+    // $('.counterOutput').empty();
+    $('').empty();
+
+$('.canvas').append(`<div class="red">
+<p class="counter">${clickCounter++}</p>
+<button id="swap">Swap</button>
+<button id="delete">Delete</button>
+</div>`);
+counterP++;
+    $("#swap").on('click', function () {
+        $(this).parent().css('background-color', 'yellow');
+    });
+    $("#delete").on('click', function () {
+        $(this).parent().css('background-color', 'yellow');
+    });
+    //clickCounter++;
+} // end createDiv
